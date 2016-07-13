63ad4b0b5bb9556e84cbbdf5b45ebb595436b2a4
Jeremy Edward Kozdon <jekozdon@nps.edu>
2016-07-13 10:44:49 -0700

    Updating example with comments and Compat

--- examples/User_Type.jl
+++ examples/User_Type.jl
@@ -1,43 +1,53 @@
+#=
+This is an example of how to use a custom user type of with ODE module.
+
+An additional example can be found in `test/inverface-tests.jl`
+
+The ODE considered in this example is the simple oscilator
+
+  a' = -b
+  b' =  a
+
+with initial condtion a(t=0) = 0 and b(t=0) = 0.1.
+=#
+
 using ODE
 using PyPlot
+using Compat
 
 typealias DataFloat Float64
-
 type Vec2
   a::DataFloat
   b::DataFloat
 end
 
-# Necessary for the solver
-Base.(:/)(x::Vec2, y::Real) = Vec2(x.a/y, x.b/y)
-Base.(:*)(y::Real, x::Vec2) = Vec2(y * x.a, y * x.b)
-Base.(:*)(x::Vec2, y::Real) = y*x
-Base.(:.*)(y::Real, x::Vec2) = y*x
-Base.(:+)(x::Vec2, y::Vec2) = Vec2(x.a + y.a, x.b + y.b)
-Base.(:-)(x::Vec2, y::Vec2) = Vec2(x.a - y.a, x.b - y.b)
+# This is the minimal set of function required on the type inorder to work with
+# the ODE module
+@compat Base.:/(x::Vec2, y::Real) = Vec2(x.a/y, x.b/y)
+@compat Base.:*(y::Real, x::Vec2) = Vec2(y * x.a, y * x.b)
+@compat Base.:*(x::Vec2, y::Real) = y*x
+@compat Base.:.*(y::Real, x::Vec2) = y*x
+@compat Base.:+(x::Vec2, y::Vec2) = Vec2(x.a + y.a, x.b + y.b)
+@compat Base.:-(x::Vec2, y::Vec2) = Vec2(x.a - y.a, x.b - y.b)
 Base.norm(x::Vec2) = sqrt(x.a^2 + x.b^2)
 Base.zero(x::Type{Vec2}) = Vec2(zero(DataFloat), zero(DataFloat))
-Base.isnan(x::Vec2) = isnan(x.a) || isnan(x.b)
+ODE.isoutofdomain(x::Vec2) = isnan(x.a) || isnan(x.b)
 
-function f(t, y)
-  (a, b) = (y.a, y.b)
-  Vec2(-b, a)
-end
+# RHS function
+f(t,y) = Vec2(-y.b, y.a)
 
-let
-  # Initial condtions
-  const start = Vec2(0.0, 0.1)
+# Initial condtions
+start = Vec2(0.0, 0.1)
 
-  # Time vector going from 0 to 2*PI in 0.01 increments
-  const time = 0:0.1:4*pi
+# Time vector going from 0 to 2*PI in 0.01 increments
+time = 0:0.1:4*pi
 
-  # Solve the ODE
-  t, y = ode45(f, start, time)
+# Solve the ODE
+t, y = ode45(f, start, time)
 
-  # Plot the solution
-  a = map(y -> y.a, y)
-  b = map(y -> y.b, y)
-  plot(t, a, label="x(t)")
-  plot(t, b, label="x'(t)")
-  legend()
-end
+# Plot the solution
+a = map(y -> y.a, y)
+b = map(y -> y.b, y)
+plot(t, a, label="a(t)")
+plot(t, b, label="b(t)")
+legend()
