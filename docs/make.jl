9e7ccfdf667812293426e135c2fb1c5caede98a7
Paweł Biernat <pawel.biernat+github@gmail.com>
2016-09-16 10:48:28 +0200

    Fixing the docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,24 @@ using Documenter, ODE
 
 makedocs(
          format = Documenter.Formats.HTML,
-         sitename = "ODE",
+         modules = [ODE],
+         clean = false,
+         sitename = "ODE.jl",
          pages = [
                   "Home" => "index.md",
 
-                  "Manual" => [ "Basics" => "man/basics.md",
-                                "Base" => "man/base.md" ]
+                  "Manual" => [
+                               "Basics" => "man/basics.md",
+                               "Base" => "man/base.md"
+                               ]
                   ]
 
          )
 
 deploydocs(
            repo = "github.com/JuliaODE/ODE.jl.git",
-           deps = Deps.pip("pygments", "python-markdown-math")
+           target = "build",
+           julia = "0.5",
+           deps = nothing,
+           make = nothing
            )
