e8c60142b534b222dfb4c1e6733e3b4ee52b0abf
Christopher Rackauckas <accounts@chrisrackauckas.com>
2017-10-04 11:19:30 -0700

    Fix saveat
    Fixes https://github.com/JuliaDiffEq/DifferentialEquations.jl/issues/206

--- src/common.jl
+++ src/common.jl
@@ -47,8 +47,11 @@ function solve{uType,tType,isinplace,AlgType<:ODEjlAlgorithm}(
     u0 = prob.u0
 
     if typeof(saveat) <: Number
-        saveat_vec = convert(Vector{tType}, saveat+tspan[1]:saveat:(tspan[end]-saveat))
-        # Exclude the endpoint because of floating point issues
+      if (tspan[1]:saveat:tspan[end])[end] == tspan[end]
+        saveat_vec = convert(Vector{tType},collect(tType,tspan[1]+saveat:saveat:tspan[end]))
+      else
+        saveat_vec = convert(Vector{tType},(collect(tType,tspan[1]+saveat:saveat:(tspan[end]-saveat)))
+      end
     else
         saveat_vec = convert(Vector{tType}, collect(saveat))
     end
